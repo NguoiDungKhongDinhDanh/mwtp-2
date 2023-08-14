5bded03efd16763f21ed69ba9396fdceda465eaf
NguoiDungKhongDinhDanh <adhominem259@gmail.com>
2023-08-15 04:22:49 +0700

    Try using a shortcut

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,13 +8,14 @@ extensions = [
 	'sphinx.ext.autosectionlabel',
 	'sphinx.ext.autosummary',
 	'sphinx.ext.duration',
-	'sphinx.ext.githubpages'
+	'sphinx.ext.githubpages',
+	'sphinx_rtd_theme'
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-html_theme = 'press'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 master_doc = 'index'
